330498d5c075e6ef4defb80b6a4071620c94aa03
Dor <dorhakim100@gmail.com>
2024-06-11 08:58:10 +0300

    Init server

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello there')
+})
+app.listen(3030, () => {
+  console.log('Server ready at port 3030')
+})
+
+app.get('/api/bug', (req, res) => {
+  console.log('works')
+})
+
+app.get('/api/bug/save', (req, res) => {
+  console.log('works')
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+  console.log('works')
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+  console.log('works')
+})
